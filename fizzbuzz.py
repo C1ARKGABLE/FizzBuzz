49d57d49e50d876d93f92d174346b2752cf4022f
Gable Brown <gbrown57@uncc.edu>
2018-12-02 02:37:12 -0500

    Cleaned up variable names

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -12,23 +12,27 @@ def main():
     print(fb(i))
 
 
-def fb(num, ls=[3,5], words=["Fizz","Buzz"]):
+def fb(number, divisors=[3,5], words=["Fizz","Buzz"]):
   '''
-  Takes three variables; an integer number (num), a list of integer divisors (ls), and a list of words (words).
+  Takes three variables; an integer number, a list of integer divisors, and a list of words.
 
-  If num is evenly divisible by a divisor at 'index' in ls, the word at 'index' in words is added to the return variable. If num is not evenly divisible by any number in ls, the original number is added as output.
+  If num is evenly divisible by a divisor at 'index' in divisors, the word at 'index' in words is added to the return variable. If num is not evenly divisible by any number in divisors, the original number is added as output.
 
   '''
-  if len(ls) != len(words):
-    raise ValueError("ls and words are not the same length but should be")
+  if len(divisors) != len(words):
+    raise ValueError("divisors and words are not the same length but should be")
 
 
   out = ""
-  for index,item in enumerate(ls):
-    if num % item == 0:
+
+  for index,divisor in enumerate(divisors):
+
+    if number % divisor == 0:
       out += words[index]
+
   if out == "":
-    out = num
+    out = number
+
   return out
 
 if __name__ == "__main__":
