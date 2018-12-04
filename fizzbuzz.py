77af5d7bae632c71bc9aeb6c2b36f2d1550a915f
Gable Brown <gbrown57@uncc.edu>
2018-12-04 16:48:29 -0500

    Added support for commandline arguments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,13 +4,31 @@ This is a Fizzbuzz script written in Python, based off of Tom Scott's video http
 Written to allow for as many words as you'd like
 '''
 
-def main():
+import sys
+
+def main(args):
   '''
   This is the main function... It just has the loop. You can add parameters if you'd like
   '''
-  for i in range(1,100):
-    print(fb(i))
+  if len(args) == 3:
 
+    divisors = []
+    for i in args[1].split(","):
+      divisors.append(int(i))
+
+    words = []
+    for i in args[2].split(","):
+      words.append(i)
+
+    for i in range(1,100):
+      print(fb(i,divisors,words))
+    print("divisors={0}, words={1}".format(divisors,words))
+
+  else:
+
+    for i in range(1,100):
+      print(fb(i))
+    print("divisors=[3,5], words=[\"Fizz\",\"Buzz\"]")
 
 def fb(number, divisors=[3,5], words=["Fizz","Buzz"]):
   '''
@@ -21,7 +39,6 @@ def fb(number, divisors=[3,5], words=["Fizz","Buzz"]):
   '''
   if len(divisors) != len(words):
     raise ValueError("divisors and words are not the same length but should be")
-
 
   out = ""
 
@@ -36,4 +53,4 @@ def fb(number, divisors=[3,5], words=["Fizz","Buzz"]):
   return out
 
 if __name__ == "__main__":
-  main()
+  main(sys.argv)
